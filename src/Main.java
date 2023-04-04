8709932cce34aab1f9b1925559fa2f5e5a0a1d37
kfcboxmaster <kambarovkuanysh@gmail.com>
2023-04-04 13:49:38 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import static java.lang.Integer.MAX_VALUE;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+        System.out.println(Util.Min(arr));
+    }
+}
+class Util{
+    static int Min(int[] arr){
+        int min = MAX_VALUE;
+        for(int i = 0 ; i < arr.length; i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
