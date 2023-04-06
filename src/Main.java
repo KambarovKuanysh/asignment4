88cd3669ae5edbc3553f7c00b5ec763fa0292711
kfcboxmaster <kambarovkuanysh@gmail.com>
2023-04-06 21:36:15 +0600

    task3: rewind(optimised, documented)

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,28 @@
 import java.util.Scanner;
-import static java.lang.Integer.MAX_VALUE;
 
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++){
-            arr[i] = in.nextInt();
-        }
-        System.out.println(Util.Min(arr));
+        System.out.println(Util.isPrime(n) ? "Prime" : "Composite");
     }
 }
 class Util{
-    static int Min(int[] arr){
-        int min = MAX_VALUE;
-        for(int i = 0 ; i < arr.length; i++){
-            if(arr[i] < min){
-                min = arr[i];
+    /*
+    @IsPrime - method takes a number and checks it for primeness using basic mathematical principles -> Number's greatest non-reminder divider its own sqrt.
+    @num - the number that's going to be checked for Primeness.
+    @return - functions returns boolean answer if number prime or composite
+     */
+    static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
             }
         }
-        return min;
+        return true;
     }
 }
