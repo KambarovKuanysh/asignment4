b1eb9e732674af462e78febc514bb92801d73b24
kfcboxmaster <kambarovkuanysh@gmail.com>
2023-04-06 21:56:27 +0600

    task4: rewind(documented)

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++){
-            arr[i] = in.nextInt();
-        }
-        System.out.println(Util.getMid(arr));
+        int num = in.nextInt();
+        System.out.println(Util.Numeral(num));
     }
 }
 class Util{
     /*
-    @getMid - function iterates through every member of an array adding it to the **num** variable. After iteration this number will be divided to the number of elements
-    in array to find middle value.
-    @arr - input array which will be calculated for middle value.
-    @num - variable which contains sum of all elements of an array. Later will be used in return to be divided and find middle.
-    @return - consists of **num** and length of an array. Returns double value of a middle of array.
+    @Numeral - recursive function which takes an integer and returns its numeral value multiplying it by (n - 1) every recursion cycle.
+Have a base condition of n == 1 to stop cycle. Example: 5 * 4 * 3 * 2 * 1
+    @n - number which will be considered numeral.
+    @return - returns n * Numeral(n - 1) every cycle until [n == 1]. Which will return 1 ending cycle.
      */
-    static double getMid(int[] arr){
-        double num = 0;
-        for(int i = 0 ; i < arr.length; i++){
-            num += arr[i];
+    static int Numeral(int n){
+        if(n == 1){
+            return 1;
         }
-        return num/arr.length;
+        return (n * Numeral(n - 1));
     }
 }
