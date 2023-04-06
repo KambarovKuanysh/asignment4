e1a04f1ba7bfa396c44021ac8179b4e4819e274e
kfcboxmaster <kambarovkuanysh@gmail.com>
2023-04-07 00:54:12 +0600

    task8: rewind(documented)

--- src/Main.java
+++ src/Main.java
@@ -3,39 +3,30 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        Util.reverseArray(num);
+        String s = scanner.next();
+        System.out.println(Util.checkIfNum(s));
     }
 }
 class Util {
-
     /*
-    @reverseArray - this method takes integer as an array length input and read array from console recursively making nested loops which on end will activate
-loops started before initializing "sout" values that stored read variables. Method uses singleton pattern for not re-initializing scanner and not lost read from console.
-    @n - length of array that will be inputted.
-    @return - program has a return statement just to end the cycle when input ends.
+    @checkIfNum - method goes through substring each recursion it has and checks its first character. Check uses ASCII table in range of ("0" - "9")
+Example: 123nur -> 23nur -> 3nur -> nur -> false.
+    @s - input string that will be checked if it is integer
+    @return - potential number will be checked by substring which will be cut at index [1]. Base condition counts on the zero length of string that will be passed.
      */
-    public static void reverseArray(int n) {
-        if (n == 0) {
-            return;
-        }
-        String num = Singleton.getScanner().next();
-        reverseArray(n - 1);
-        System.out.print(num + " ");
-    }
-}
+    public static boolean checkIfNum(String s) {
 
-class Singleton {
-    private static Scanner scanner;
-    private Singleton() {}
-    /*
-    This pattern was implemented due lost of data when re-initializing scanner. By this way we can make sure that scanner always will be the same.
-     */
-
-    public static synchronized Scanner getScanner() {
-        if (scanner == null) {
-            scanner = new Scanner(System.in);
+        if (s.length() == 0) {
+            return true;
         }
-        return scanner;
+
+        char firstChar = s.charAt(0);
+
+        if (firstChar >= '0' && firstChar <= '9') {
+            return checkIfNum(s.substring(1));
+
+        } else {
+            return false;
+        }
     }
 }
