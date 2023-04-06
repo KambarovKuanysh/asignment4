3c1a16813d1ac21efdacc89468bdeb8dd63fae08
kfcboxmaster <kambarovkuanysh@gmail.com>
2023-04-06 22:11:06 +0600

    task6: rewind(optimised, documented)

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int num = in.nextInt();
-        System.out.println(Util.fibonacci(num));
+        int a = in.nextInt();
+        int n = in.nextInt();
+        System.out.println(Util.power(a, n));
     }
 }
 class Util{
     /*
-    @fibonacci - function that return [n] element of fibonacci cycle. Function uses basic formula of fibonacci member. F(2n) = F(1n) + F(0n).
-every fibonacci member is compounded from "ones" which is first and second fibonacci member. Program disassemble every number into primal "ones".
-Example: 1 + 1 + 1 + 1 + 1 = 2 + 3.
-    @n - order of number that will be searched in fibonacci cycle.
-    @return - uses formula of fibonacci number( F(2n) = F(1n) + F(0n) ) to recursively disassemble numbers and ends at a base condition of first
-or second fibonacci member(1,1).
+    @power - function that takes two values [a - number], [n - power] and returns a^n. Return multiplies itself every recursion(n - 1) and end at base condition of n == 0.
+    @a - number that will be added power.
+    @n - power that will be graded for number.
+    @return - every return statement multiplies number by itself until condition n == 0 meets. Recursion progresses by subtracting n(n - 1).
      */
-    static int fibonacci(int n){
-        if(n <= 2){
+    static int power(int a, int n){
+        if(n == 0){
             return 1;
         }
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        return a * power(a, n - 1);
     }
 }
