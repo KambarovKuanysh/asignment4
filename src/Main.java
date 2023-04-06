d2dd1c1fd2ba63afb31355204129c1cf0e381a5b
kfcboxmaster <kambarovkuanysh@gmail.com>
2023-04-07 01:08:55 +0600

    task9: rewind(documented)

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,25 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String s = scanner.next();
-        System.out.println(Util.checkIfNum(s));
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        System.out.println(Util.countBinaryCombo(k,n));
     }
 }
 class Util {
     /*
-    @checkIfNum - method goes through substring each recursion it has and checks its first character. Check uses ASCII table in range of ("0" - "9")
-Example: 123nur -> 23nur -> 3nur -> nur -> false.
-    @s - input string that will be checked if it is integer
-    @return - potential number will be checked by substring which will be cut at index [1]. Base condition counts on the zero length of string that will be passed.
+    @countBinaryCombo - method counts possible binary combination and uses 2 fundamental formulas. Cnk=Cn−1k−1+Cn−1k where Cn0=Cnn=1. Every binary combination can be
+disassembled to "ones". That's what this method does. It primarises counting.
+    @k - integer value for counts of pick.
+    @n - possible variants.
+    @return - program uses given formula to disassemble. It will continue returning formula until it will meet base condition Cn0=Cnn=1 and then add it to return integer sum.
      */
-    public static boolean checkIfNum(String s) {
-
-        if (s.length() == 0) {
-            return true;
+    public static int countBinaryCombo(int k, int n) {
+        if(k == 0 || k == n){
+            return 1;
         }
-
-        char firstChar = s.charAt(0);
-
-        if (firstChar >= '0' && firstChar <= '9') {
-            return checkIfNum(s.substring(1));
-
-        } else {
-            return false;
+        else {
+            return countBinaryCombo(k - 1, n - 1) + countBinaryCombo(k , n - 1);
         }
     }
 }
